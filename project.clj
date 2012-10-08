2f923e2777a47642909f57f5d305066098adb5ca
Heehong Moon <bbirec@gmail.com>
2012-10-09 01:33:17 +0900

    .

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject cs499 "0.1.0-SNAPSHOT"
   :description "CS499 Web site"
   :url "http://cs499.bbirec.com"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-time "0.4.4"]
                  [noir "1.2.1"]
                  [factual/factual-clojure-driver "1.4.3"]
                  [factql "1.0.3"]
-                 [com.novemberain/monger "1.2.0"]]
+                 [com.novemberain/monger "1.1.2"]]
   :main cs499.server)

75016be6362758021872f6f6664d6e76eecbf770
Heehong Moon <bbirec@gmail.com>
2012-10-23 20:46:38 +0900

    Fixed.

--- src/cs499/ggnnq.clj
+++ src/cs499/ggnnq.clj
@@ -10,11 +10,18 @@
                  #(vector (rand-int bound) (rand-int bound))))))
 
 
+(defn round-places
+  "Round the float number to have the given number of decimals"
+  [number decimals]
+  (let [factor (math/expt 10 decimals)]
+    (bigdec (/ (math/round (* factor number)) factor))))
 
 (defn dist
   "Euclidean distance between two points."
   [p1 p2]
-  (math/sqrt (reduce + (map #(math/expt (- %1 %2) 2) p1 p2))))
+  (round-places (math/sqrt (reduce +
+                                   (map #(math/expt (- %1 %2) 2) p1 p2)))
+                2))
 
 (defn dist-pair
   "Distance of point pair"
@@ -63,22 +70,6 @@
           (list item)
           (take-last (- (count coll) idx) coll)))
 
-(defn calc-score
-  "Calculating the score. The idx is the index of query set which has the q point."
-  [pair idx query-sets]
-  (let [p (first pair)
-        q (second pair)
-        qs (keep-except idx query-sets)
-        closest-pair (map #(vector p (nearest-point p %1)) qs)
-        closest-dist (map dist-pair closest-pair)
-        query-points (into [] (insert-item (map second closest-pair)
-                                   idx
-                                   q))]
-    [(+ (dist-pair pair) (reduce + closest-dist))
-     p
-     query-points]))
-
-
 (defn rac
   ([nearest-pairs col row]
      (nth (nth nearest-pairs col) row))
@@ -110,14 +101,8 @@
         remains (keep-except col nearest-pairs)
         founds (map #(find-pairs % row p) remains)
         possibles (apply comb/cartesian-product founds)]
-    (prn "Pair:" (pr-str pair) " Possibles:" (pr-str possibles))
+    #_(prn "Pair:" (pr-str pair) " Possibles:" (pr-str possibles))
     (map #(apply make-result-form pair col %) possibles)))
-
-(defn min-index [coll]
-  (first (apply min-key second (map-indexed vector coll))))
-
-(defn max-index [coll]
-  (first (apply max-key second (map-indexed vector coll))))
 
 
 (defn new-result-set
@@ -125,8 +110,23 @@
   [s k results]
   (apply sorted-set
          (take k (apply sorted-set-by
-                        #(< (first %1) (first %2))
+                        #(<= (first %1) (first %2))
                         (clojure.set/union s results)))))
+
+(defn threshold
+  [nearest-pairs row col]
+  (let [pair (rac nearest-pairs col row)
+        remains (keep-except col nearest-pairs)
+        first-pairs (map first remains)
+        first-dists (map #(dist-pair %) first-pairs)]
+    (+ (dist-pair pair)
+       (reduce + first-dists))))
+
+(defn find-threshold
+  [nearest-pairs row]
+  (let [ths (map #(threshold nearest-pairs row %)
+                 (range (count nearest-pairs)))]
+    (apply min ths)))
 
 (defn ggnnq
   "GGNNQ"
@@ -135,11 +135,9 @@
         threshold (atom 0)
         result-set (atom #{})
         idx (atom 0)]
-    (prn "NNP" (pr-str nearest-pairs))
+    #_(prn "NNP" (pr-str nearest-pairs))
     (while (and (or (empty? @result-set)
                     (< (count @result-set) k)
-                    #_true
-                    #_(some #(<= @threshold %) (map first @result-set))
                     (and
                      (= (count @result-set) k)
                      (< @threshold (apply max (map first @result-set)))))
@@ -155,17 +153,26 @@
                                 (map #(find-candidates nearest-pairs @idx %)
                                      (range (count query-sets))))]
 
-          (prn "New results:" (pr-str candidates))
+          #_(prn "Candidates:" (pr-str candidates))
+          #_(if (some #{[62.23M [52 54] [[41 50] [66 38] [81 50]]]}
+                    candidates)
+            (prn (pr-str candidates)))
           
           ;; Adding to the final set
           (reset! result-set
                   (new-result-set @result-set
                                   k
                                   (apply sorted-set candidates)))
-          (prn "Now result map has " (str (count @result-set)))
+          #_(prn "Now result map has " (str (count @result-set)))
+
+          #_(if (some #{[62.23M [52 54] [[41 50] [66 38] [81 50]]]}
+                    candidates)
+            (prn (pr-str @result-set)))
+
           
           ;; Set threshold
-          (let [t (apply min (map first candidates))
+          (let [t (find-threshold nearest-pairs @idx)
+                #_(apply min (map first candidates))
                 #_(reduce + (map #(dist-pair %) next-pairs))]
             (prn (str "T:" t))
             (reset! threshold t)))
@@ -173,9 +180,6 @@
         ;; Increment idx
         (swap! idx inc)))
     @result-set))
-
-
-
 
 
 ;; Brute force algorithm
@@ -197,16 +201,34 @@
                             points))]
     (apply sorted-set (take k (sort-by first < results)))))
 
-(defn do-test2
-  ([k P & Q]
-     (let [r1 (apply brute-force-ggnnq k P Q)
-           r2 (apply ggnnq k P Q)]
-       (prn (pr-str r1))
-       (prn (pr-str r2))
-       (= r1 r2))))
+
+;; Test
+
+(defn check-result [r1 r2]
+  (prn (pr-str r1))
+  (prn (pr-str r2))
+  (if (= r1 r2)
+    true
+    (do
+      (prn (pr-str (clojure.set/difference r1 r2)))
+      (prn (pr-str (clojure.set/difference r2 r1)))
+      false)))
+
+
+(def lp
+  (atom
+   #{[33 1] [31 0] [52 54] [20 57] [23 68] [25 15] [35 91] [71 64] [23 51] [2 64]}))
+(def lq1
+  (atom
+   #{[31 66] [30 4] [41 50] [79 27] [63 11] [6 83] [31 16] [34 85] [17 36] [57 20]}))
+(def lq2
+  (atom
+   #{[41 13] [7 75] [93 1] [95 3] [66 38] [59 6] [69 19] [91 83] [56 18] [90 88]}))
+(def lq3
+  (atom
+   #{[77 45] [81 50] [76 46] [13 84] [55 32] [36 49] [36 19] [12 27] [70 22] [58 77]}))
 
 (defn do-test
-  
   ([k p-size q-count q-size]
      (let [p (gen-random-set p-size 100)
            q1 (gen-random-set q-size 100)
@@ -214,8 +236,15 @@
            q3 (gen-random-set q-size 100)
            r1 (brute-force-ggnnq k p q1 q2 q3)
            r2 (ggnnq k p q1 q2 q3)]
-       (prn (pr-str r1))
-       (prn (pr-str r2))
-       (= r1 r2))))
+       (reset! lp p)
+       (reset! lq1 q1)
+       (reset! lq2 q2)
+       (reset! lq3 q3)
+       (check-result r1 r2))))
 
 
+(defn do-test2
+  ([k P & Q]
+     (let [r1 (apply brute-force-ggnnq k P Q)
+           r2 (apply ggnnq k P Q)]
+       (check-result r1 r2))))
